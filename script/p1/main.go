ed37e8d0189d7d3d66e69c4369121adf9a881429
gepengcheng <gepengcheng@richisland.com>
2023-07-24 17:31:07 +0800

    fix -> 第一章

---
+++ script/p1/main.go
@@ -0,0 +1,15 @@
+/*
+ * @Description:
+ * @Author: gepengcheng
+ * @Date: 2023-07-24 16:57:02
+ * @LastEditors: gepengcheng
+ * @LastEditTime: 2023-07-24 17:16:06
+ */
+package main
+
+import "fmt"
+
+
+func main() {
+	fmt.Println("Hello World!")
+}
